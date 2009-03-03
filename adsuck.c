5de60a4f64583f91c5fab22f3e469f2c9b16fd3a
Marco Peereboom <marco@conformal.com>
2009-03-03 22:41:22 +0000

    Major major cleanup.

--- adsuck.c
+++ adsuck.c
@@ -27,6 +27,14 @@ int			entries;
 int			verbose;
 int			debug;
 
+/* socket */
+int			sock;
+struct sockaddr		paddr;
+socklen_t		plen = (socklen_t) sizeof(paddr);
+
+/* resolver */
+ldns_resolver		*res;
+
 extern char		*__progname;
 
 struct hostnode {
@@ -130,11 +138,6 @@ hostnamefrompkt(ldns_pkt *pkt, ldns_rr **qrr)
 		return (NULL);
 
 	query_rr = ldns_rr_list_rr(ldns_pkt_question(pkt), 0);
-	if (debug) {
-		printf("QUERY RR:\n");
-		ldns_rr_print(stdout, query_rr);
-	}
-
 	rawname = ldns_rr2str(query_rr);
 	if (rawname == NULL)
 		goto done;
@@ -164,18 +167,171 @@ done:
 }
 
 int
+spoofquery(char *hostname, ldns_rr *query_rr, u_int16_t id)
+{
+	ldns_status		status;
+	ldns_rr			*myrr = NULL, *myaurr = NULL;
+	ldns_rdf		*prev = NULL;
+	ldns_rr_list		*answer_an = NULL;
+	ldns_rr_list		*answer_ns = NULL;
+	ldns_rr_list		*answer_ad = NULL;
+	ldns_rr_list		*answer_qr = NULL;
+	ldns_pkt		*answer_pkt = NULL;
+	size_t			answer_size;
+	uint8_t			*outbuf = NULL;
+	char			buf[128];
+	int			rv = 1;
+
+	/* answer section */
+	answer_an = ldns_rr_list_new();
+	snprintf(buf, sizeof buf, "%s.\t%d\tIN\tA\t127.0.0.1",
+	    hostname, 259200);
+	status = ldns_rr_new_frm_str(&myrr, buf, 0, NULL, &prev);
+	if (status != LDNS_STATUS_OK) {
+		fprintf(stderr, "can't create answer section: %s\n",
+		    ldns_get_errorstr_by_id(status));
+		goto unwind;
+	}
+	ldns_rr_list_push_rr(answer_an, myrr);
+	ldns_rdf_deep_free(prev);
+	prev = NULL;
+
+	/* authority section */
+	answer_ns = ldns_rr_list_new();
+	snprintf(buf, sizeof buf, "%s.\t%d\tIN\tNS\t127.0.0.1.",
+	    hostname, 259200);
+	status = ldns_rr_new_frm_str(&myaurr, buf, 0, NULL, &prev);
+	if (status != LDNS_STATUS_OK) {
+		fprintf(stderr, "can't create authority section: %s\n",
+		    ldns_get_errorstr_by_id(status));
+		goto unwind;
+	}
+	ldns_rr_list_push_rr(answer_ns, myaurr);
+	ldns_rdf_deep_free(prev);
+	prev = NULL;
+
+	/* question section */
+	answer_qr = ldns_rr_list_new();
+	ldns_rr_list_push_rr(answer_qr, ldns_rr_clone(query_rr));
+
+	/* actual packet */
+	answer_pkt = ldns_pkt_new();
+	answer_ad = ldns_rr_list_new();
+	
+	ldns_pkt_set_qr(answer_pkt, 1);
+	ldns_pkt_set_aa(answer_pkt, 1);
+	ldns_pkt_set_id(answer_pkt, id);
+
+	ldns_pkt_push_rr_list(answer_pkt, LDNS_SECTION_QUESTION, answer_qr);
+	ldns_pkt_push_rr_list(answer_pkt, LDNS_SECTION_ANSWER, answer_an);
+	ldns_pkt_push_rr_list(answer_pkt, LDNS_SECTION_AUTHORITY, answer_ns);
+	ldns_pkt_push_rr_list(answer_pkt, LDNS_SECTION_ADDITIONAL, answer_ad);
+
+	status = ldns_pkt2wire(&outbuf, answer_pkt, &answer_size);
+	if (status != LDNS_STATUS_OK)
+		fprintf(stderr, "can't create answer: %s\n",
+		    ldns_get_errorstr_by_id(status));
+	else {
+		if (debug) {
+			fprintf(stderr, "spoofquery response:\n");
+			ldns_pkt_print(stderr, answer_pkt);
+		}
+
+		if (sendto(sock, outbuf, answer_size, 0, &paddr, plen) == -1)
+			warn("spoofquery sendto");
+		else {
+			rv = 0;
+			if (verbose)
+				fprintf(stderr, "spoofquery: spoofing %s\n",
+				    hostname);
+		}
+	}
+
+unwind:
+	if (answer_pkt)
+		ldns_pkt_free(answer_pkt);
+	if (outbuf)
+		LDNS_FREE(outbuf);
+	if (answer_qr)
+		ldns_rr_list_free(answer_qr);
+	if (answer_an)
+		ldns_rr_list_free(answer_an);
+	if (answer_ns)
+		ldns_rr_list_free(answer_ns);
+	if (answer_ad)
+		ldns_rr_list_free(answer_ad);
+
+	return (rv);
+}
+
+int
+forwardquery(char *hostname, ldns_rr *query_rr, u_int16_t id)
+{
+	size_t			answer_size;
+	u_int16_t		qflags = LDNS_RD;
+	uint8_t			*outbuf = NULL;
+	ldns_rdf		*qname = NULL;
+	ldns_pkt		*respkt = NULL;
+	ldns_rr_type		type;
+	ldns_rr_class		clas;
+	ldns_status		status;
+	int			rv = 1;
+
+	qname = ldns_dname_new_frm_str(hostname);
+	if (!qname) {
+		fprintf(stderr, "forwardquery: can't make qname\n");
+		goto unwind;
+	}
+	type = ldns_rr_get_type(query_rr);
+	clas = ldns_rr_get_class(query_rr);
+	respkt = ldns_resolver_query(res, qname, type, clas, qflags);
+	if (respkt == NULL) {
+		fprintf(stderr, "forwardquery: no respkt\n");
+		goto unwind;
+	}
+	if (debug) {
+		fprintf(stderr, "forwardquery response:\n");
+		ldns_pkt_print(stderr, respkt);
+	}
+
+	ldns_pkt_set_id(respkt, id);
+	status = ldns_pkt2wire(&outbuf, respkt, &answer_size);
+	if (status != LDNS_STATUS_OK)
+		fprintf(stderr, "can't create answer: %s\n",
+		    ldns_get_errorstr_by_id(status));
+	else {
+		if (sendto(sock, outbuf, answer_size, 0, &paddr, plen) == -1)
+			warn("forwardquery sendto");
+		else {
+			rv = 0;
+			if (verbose)
+				fprintf(stderr, "forwardquery: resolved %s\n",
+				    hostname);
+		}
+	}
+
+unwind:
+	if (respkt)
+		ldns_pkt_free(respkt);
+	if (outbuf)
+		LDNS_FREE(outbuf);
+	if (qname)
+		ldns_rdf_free(qname);
+
+	return (rv);
+}
+
+int
 main(int argc, char *argv[])
 {
-	int			c, sock;
+	int			c;
 	ssize_t			nb;
 	uint8_t			inbuf[INBUF_SIZE];
-	struct sockaddr		addr_him;
-	socklen_t		hislen = (socklen_t) sizeof(addr_him);
+	u_int16_t		id;
 	ldns_status		status;
 	ldns_pkt		*query_pkt;
 	struct hostnode		hostn, *n;
 	ldns_rr			*query_rr;
-	ldns_resolver		*res;
 	char			*resolv_conf = NULL;
 
 	while ((c = getopt(argc, argv, "df:v")) != -1) {
@@ -221,123 +377,31 @@ main(int argc, char *argv[])
 	}
 
 	for (;;) {
-		nb = recvfrom(sock, inbuf, INBUF_SIZE, 0, &addr_him, &hislen);
-		if (nb == -1)
-			err(1, "can't recvfrom");
+		nb = recvfrom(sock, inbuf, INBUF_SIZE, 0, &paddr, &plen);
+		if (nb == -1) {
+			if (errno == EINTR || errno == EAGAIN)
+				continue;
+			else
+				err(1, "recvfrom");
+		}
 
 		status = ldns_wire2pkt(&query_pkt, inbuf, (size_t)nb);
 		if (status != LDNS_STATUS_OK) {
-			printf("bad packet: %s\n", ldns_get_errorstr_by_id(status));
+			fprintf(stderr, "bad packet: %s\n",
+			    ldns_get_errorstr_by_id(status));
 			continue;
 		} else
-			if (debug)
-				ldns_pkt_print(stdout, query_pkt);
+			if (debug) {
+				fprintf(stderr, "received packet:\n");
+				ldns_pkt_print(stderr, query_pkt);
+			}
 
 		hostn.hostname = hostnamefrompkt(query_pkt, &query_rr);
-		if ((n = RB_FIND(hosttree, &hosthead, &hostn)) != NULL) {
-			ldns_rr		*myrr = NULL, *myaurr = NULL;
-			ldns_rdf	*prev = NULL;
-			ldns_rr_list	*answer_an;
-			ldns_rr_list	*answer_ns;
-			ldns_rr_list	*answer_ad;
-			ldns_rr_list	*answer_qr;
-			ldns_pkt	*answer_pkt;
-			size_t		answer_size;
-			uint8_t		*outbuf;
-			char		buf[128];
-
-			/* answer section */
-			answer_an = ldns_rr_list_new();
-			snprintf(buf, sizeof buf, "%s.\t%d\tIN\tA\t127.0.0.1", hostn.hostname, 259200);
-			printf("a buf: %s\n", buf);
-			status = ldns_rr_new_frm_str(&myrr, buf, 0, NULL, &prev);
-			if (status != LDNS_STATUS_OK) {
-				printf("can't create answer section: %s\n", ldns_get_errorstr_by_id(status));
-				/* free some memory */
-				continue;
-			}
-			ldns_rr_list_push_rr(answer_an, myrr);
-			ldns_rdf_deep_free(prev);
-			prev = NULL;
-
-			/* authority section */
-			answer_ns = ldns_rr_list_new();
-			snprintf(buf, sizeof buf, "%s.\t%d\tIN\tNS\t127.0.0.1.", hostn.hostname, 259200);
-			printf("ns buf: %s\n", buf);
-			status = ldns_rr_new_frm_str(&myaurr, buf, 0, NULL, &prev);
-			if (status != LDNS_STATUS_OK) {
-				printf("can't create authority section: %s\n", ldns_get_errorstr_by_id(status));
-				continue;
-			}
-			ldns_rr_list_push_rr(answer_ns, myaurr);
-			ldns_rdf_deep_free(prev);
-			prev = NULL;
-
-			/* question section */
-			answer_qr = ldns_rr_list_new();
-			ldns_rr_list_push_rr(answer_qr, ldns_rr_clone(query_rr));
-
-			/* actual packet */
-			answer_pkt = ldns_pkt_new();
-			answer_ad = ldns_rr_list_new();
-			
-			ldns_pkt_set_qr(answer_pkt, 1);
-			ldns_pkt_set_aa(answer_pkt, 1);
-			ldns_pkt_set_id(answer_pkt, ldns_pkt_id(query_pkt));
-
-			ldns_pkt_push_rr_list(answer_pkt, LDNS_SECTION_QUESTION, answer_qr);
-			ldns_pkt_push_rr_list(answer_pkt, LDNS_SECTION_ANSWER, answer_an);
-			ldns_pkt_push_rr_list(answer_pkt, LDNS_SECTION_AUTHORITY, answer_ns);
-			ldns_pkt_push_rr_list(answer_pkt, LDNS_SECTION_ADDITIONAL, answer_ad);
-
-			status = ldns_pkt2wire(&outbuf, answer_pkt, &answer_size);
-			if (status != LDNS_STATUS_OK)
-				printf("can't create answer: %s\n", ldns_get_errorstr_by_id(status));
-			else
-				nb = sendto(sock, outbuf, answer_size, 0, &addr_him, hislen);
-
-			ldns_pkt_free(answer_pkt);
-			LDNS_FREE(outbuf);
-			ldns_rr_list_free(answer_qr);
-			ldns_rr_list_free(answer_an);
-			ldns_rr_list_free(answer_ns);
-			ldns_rr_list_free(answer_ad);
-
-			printf("found: %s\n", hostn.hostname);
-		} else {
-			uint8_t		*outbuf;
-			size_t		answer_size;
-			u_int16_t	qflags = LDNS_RD;
-			ldns_rdf	*qname;
-			ldns_pkt	*respkt;
-			ldns_rr_type	type;
-			ldns_rr_class	clas;
-
-			printf("resolving: %s\n", hostn.hostname);
-			qname = ldns_dname_new_frm_str(hostn.hostname);
-			if (!qname)
-				errx(1, "can't make qname\n"); /* XXX do something less drstic */
-			type = ldns_rr_get_type(query_rr);
-			clas = ldns_rr_get_class(query_rr);
-			respkt = ldns_resolver_query(res, qname, type, clas, qflags);
-			if (respkt == NULL)
-				errx(1, "no respkt");
-			if (verbose)
-				ldns_pkt_print(stdout, respkt);
-
-			ldns_pkt_set_id(respkt, ldns_pkt_id(query_pkt));
-			status = ldns_pkt2wire(&outbuf, respkt, &answer_size);
-			if (status != LDNS_STATUS_OK)
-				printf("can't create answer: %s\n", ldns_get_errorstr_by_id(status));
-			else
-				nb = sendto(sock, outbuf, answer_size, 0, &addr_him, hislen);
-
-			ldns_pkt_free(respkt);
-			LDNS_FREE(outbuf);
-			ldns_rdf_free(qname);
-
-			printf("sent\n");
-		}
+		id = ldns_pkt_id(query_pkt);
+		if ((n = RB_FIND(hosttree, &hosthead, &hostn)) != NULL)
+			spoofquery(hostn.hostname, query_rr, id);
+		else
+			forwardquery(hostn.hostname, query_rr, id);
 
 		free(hostn.hostname);
 		ldns_pkt_free(query_pkt);
